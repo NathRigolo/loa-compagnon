60ba8bd33e900b3ca12358414c58fae4970febda
NathRigolo <graasnath@gmail.com>
2026-05-30 11:28:48 +0200

    0.9.3

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Cache les assets statiques pour fonctionner hors-ligne.
    Bumper CACHE_NAME quand on déploie une nouvelle version. */
 
-const CACHE_NAME = 'loa-compagnon-v15';
+const CACHE_NAME = 'loa-compagnon-v16';
 const ASSETS = [
   './',
   './index.html',
